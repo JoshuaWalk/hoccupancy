1aa2e69b097fe923d296c01414e2886396b46f49
Ram Su <catmorte@gmail.com>
2020-03-30 15:42:28 +0300

    qq fix detailed

--- src/views/Detailed.vue
+++ src/views/Detailed.vue
@@ -44,19 +44,19 @@
             <div class="control">
               <span class="tags has-addons" v-if="location.statistics.status">
                 <span class="tag is-medium">Last 24h</span>
-                <span class="tag is-success">{{
+                <span class="tag is-success is-medium">{{
                   location.statistics.amounts.g?location.statistics.amounts.g:'0'
                 }}</span>
-                <span class="tag is-warning">{{
+                <span class="tag is-warning is-medium">{{
                   location.statistics.amounts.y?location.statistics.amounts.y:'0'
                 }}</span>
-                <span class="tag is-danger">{{
+                <span class="tag is-danger is-medium">{{
                   location.statistics.amounts.r?location.statistics.amounts.r:'0'
                 }}</span>
               </span>
-              <span class="tags has-addons is-medium" v-else>
-                <span class="tag">Last 24h</span>
-                <span class="tag ">No data</span>
+              <span class="tags has-addons " v-else>
+                <span class="tag is-medium">Last 24h</span>
+                <span class="tag is-medium">No data</span>
               </span>
             </div>
           </div>
